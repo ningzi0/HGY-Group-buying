837d8bf6a609b41ffa436095d94d499023633356
Your Name <you@example.com>
2023-04-25 16:11:10 +0800

    个人资料

--- pages/usercenter/person-info/index.js
+++ pages/usercenter/person-info/index.js
@@ -1,9 +1,39 @@
-import { fetchPerson } from '../../../services/usercenter/fetchPerson';
-import { phoneEncryption } from '../../../utils/util';
+import {
+  fetchPerson
+} from '../../../services/usercenter/fetchPerson';
+import {
+  phoneEncryption
+} from '../../../utils/util';
 import Toast from 'tdesign-miniprogram/toast/index';
+import {
+  areaData
+} from '../../../config/index';
+
 
 Page({
   data: {
+    locationState: {
+      labelIndex: null,
+      addressId: '',
+      addressTag: '',
+      cityCode: '',
+      cityName: '',
+      countryCode: '',
+      countryName: '',
+      detailAddress: '',
+      districtCode: '',
+      districtName: '',
+      isDefault: false,
+      name: '',
+      phone: '',
+      provinceCode: '',
+      provinceName: '',
+      isEdit: false,
+      isOrderDetail: false,
+      isOrderSure: false,
+    },
+    areaData: areaData,
+    areaPickerVisible: false,
     personInfo: {
       avatarUrl: '',
       nickName: '',
@@ -11,8 +41,7 @@ Page({
       phoneNumber: '',
     },
     showUnbindConfirm: false,
-    pickerOptions: [
-      {
+    pickerOptions: [{
         name: '男',
         code: '1',
       },
@@ -38,9 +67,15 @@ Page({
       });
     });
   },
-  onClickCell({ currentTarget }) {
-    const { dataset } = currentTarget;
-    const { nickName } = this.data.personInfo;
+  onClickCell({
+    currentTarget
+  }) {
+    const {
+      dataset
+    } = currentTarget;
+    const {
+      nickName
+    } = this.data.personInfo;
 
     switch (dataset.type) {
       case 'gender':
@@ -67,9 +102,10 @@ Page({
     });
   },
   onConfirm(e) {
-    const { value } = e.detail;
-    this.setData(
-      {
+    const {
+      value
+    } = e.detail;
+    this.setData({
         typeVisible: false,
         'personInfo.gender': value,
       },
@@ -91,11 +127,16 @@ Page({
           sizeType: ['compressed'],
           sourceType: ['album', 'camera'],
           success: (res) => {
-            const { path, size } = res.tempFiles[0];
+            const {
+              path,
+              size
+            } = res.tempFiles[0];
             if (size <= 10485760) {
               resolve(path);
             } else {
-              reject({ errMsg: '图片大小超出限制，请重新上传' });
+              reject({
+                errMsg: '图片大小超出限制，请重新上传'
+              });
             }
           },
           fail: (err) => reject(err),
@@ -119,4 +160,142 @@ Page({
       });
     }
   },
+  onInputValue(e) {
+    const {
+      item
+    } = e.currentTarget.dataset;
+    if (item === 'address') {
+      const {
+        selectedOptions = []
+      } = e.detail;
+      this.setData({
+          'locationState.provinceCode': selectedOptions[0].value,
+          'locationState.provinceName': selectedOptions[0].label,
+          'locationState.cityName': selectedOptions[1].label,
+          'locationState.cityCode': selectedOptions[1].value,
+          'locationState.districtCode': selectedOptions[2].value,
+          'locationState.districtName': selectedOptions[2].label,
+          areaPickerVisible: false,
+        },
+        () => {
+          const {
+            isLegal,
+            tips
+          } = this.onVerifyInputLegal();
+          this.setData({
+            submitActive: isLegal,
+          });
+          this.privateData.verifyTips = tips;
+        },
+      );
+    } else {
+      const {
+        value = ''
+      } = e.detail;
+      this.setData({
+          [`locationState.${item}`]: value,
+        },
+        () => {
+          const {
+            isLegal,
+            tips
+          } = this.onVerifyInputLegal();
+          this.setData({
+            submitActive: isLegal,
+          });
+          this.privateData.verifyTips = tips;
+        },
+      );
+    }
+  },
+  onPickArea() {
+    console.log(1)
+    this.setData({
+      areaPickerVisible: true
+    });
+  },
+  onSearchAddress() {
+    this.builtInSearch({
+      code: 'scope.userLocation',
+      name: '地址位置'
+    }).then(() => {
+      wx.chooseLocation({
+        success: (res) => {
+          if (res.name) {
+            this.triggerEvent('addressParse', {
+              address: res.address,
+              name: res.name,
+              latitude: res.latitude,
+              longitude: res.longitude,
+            });
+          } else {
+            Toast({
+              context: this,
+              selector: '#t-toast',
+              message: '地点为空，请重新选择',
+              icon: '',
+              duration: 1000,
+            });
+          }
+        },
+        fail: function (res) {
+          console.warn(`wx.chooseLocation fail: ${JSON.stringify(res)}`);
+          if (res.errMsg !== 'chooseLocation:fail cancel') {
+            Toast({
+              context: this,
+              selector: '#t-toast',
+              message: '地点错误，请重新选择',
+              icon: '',
+              duration: 1000,
+            });
+          }
+        },
+      });
+    });
+  },
+  builtInSearch({
+    code,
+    name
+  }) {
+    return new Promise((resolve, reject) => {
+      wx.getSetting({
+        success: (res) => {
+          if (res.authSetting[code] === false) {
+            wx.showModal({
+              title: `获取${name}失败`,
+              content: `获取${name}失败，请在【右上角】-小程序【设置】项中，将【${name}】开启。`,
+              confirmText: '去设置',
+              confirmColor: '#FA550F',
+              cancelColor: '取消',
+              success(res) {
+                if (res.confirm) {
+                  wx.openSetting({
+                    success(settinRes) {
+                      if (settinRes.authSetting[code] === true) {
+                        resolve();
+                      } else {
+                        console.warn('用户未打开权限', name, code);
+                        reject();
+                      }
+                    },
+                  });
+                } else {
+                  reject();
+                }
+              },
+              fail() {
+                reject();
+              },
+            });
+          } else {
+            resolve();
+          }
+        },
+        fail() {
+          reject();
+        },
+      });
+    });
+  },
+
 });
